75df632753c76732c5dde1ce1568d1f4151214c2
Nitish <neelgiri65@gmail.com>
2023-06-12 04:12:26 +0530

    jwt

---
+++ SpringSecurity-RoleJWT/src/test/java/com/masai/SpringSecurityApp/RoleBasedAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai.SpringSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
